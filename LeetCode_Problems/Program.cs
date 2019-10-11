afc0364107251ea11265d1f65d4769ff48d7b12d
Prakul Garg (HCL America Inc) <v-praga@microsoft.com>
2019-10-11 12:37:37 +0530

    Add project files.

---
+++ LeetCode_Problems/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution to NQueens Problem");
+
+            // MarkNQueens Problem
+            int n = 5;
+
+            NQueensProblem problem = new NQueensProblem();
+            IList<int[,]> solutions = problem.GetPossibleSolutions(n);
+
+            Console.WriteLine("# of paussible solutions" + solutions.Count);
+
+            int iLoop = 0;
+            foreach (int[,] solution in solutions)
+            {
+                Console.WriteLine("============ {0} ===============", ++iLoop);
+                PrintResults(solution, n);
+                Console.WriteLine("===========================");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void PrintResults(int[,] solution, int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(solution[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
